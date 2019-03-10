9fc54fa10bab1aa01d7f91afb706909afc7444d4
yevtushenkooleg <yevtushenkooleg@gmail.com>
2019-03-10 20:45:51 +0200

    HW4

---
+++ HW4.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var c=15, d=2;
+var result=c+d;
+    alert (result); //задание "2."
+
+var a=10,b=2,c=5;
+    alert (a+b+c); //задание "3."
+
+var a=17,b=10;
+var c=a-b,d=7;
+var result=c+d;
+    alert (result); //задания "4. и 5."
+
+var str='Привет, Мир!';
+    alert (str); //задание "6."
+
+var str1='Привет, ',str2='Мир!';
+    alert (str1+str2); //задание "7."
+
+var name='Oleg';
+    alert ("Привет, " +name); //задание "8."
+
+var age='27';
+    alert ('Мне '+age+" лет"); //задание "9."
+
